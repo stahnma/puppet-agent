19a4be59258a6f684ccf1ce16ab2ad681e02893b
Michael Stahnke <stahnma@puppetlabs.com>
2015-03-17 19:30:00 +0000

    WIP

--- configs/components/passenger-apache-conf.rb
+++ configs/components/passenger-apache-conf.rb
@@ -1,5 +1,5 @@
 component "passenger-apache.conf" do |pkg, settings, platform|
   pkg.version "0.0.0"
-  pkg.md5sum "cbfa1dec5e3710d65243720524598e47"
+  pkg.md5sum "d2dfdadca04d50ae4f57f273ca9cf835"
   pkg.url "http://buildsources.delivery.puppetlabs.net/passenger-apache.conf"
 end
