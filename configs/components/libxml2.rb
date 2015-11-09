a19c8df0f5eaa2be16c49f57a1da9d5c57bbf8b9
Michael Stahnke <stahnma@puppetlabs.com>
2015-11-09 17:39:58 +0000

    (PA-45) Add ability to build libxml2 for AIX
    AIX specific implementation features added.

--- configs/components/libxml2.rb
+++ configs/components/libxml2.rb
@@ -3,11 +3,16 @@ component "libxml2" do |pkg, settings, platform|
   pkg.md5sum "9e6a9aca9d155737868b3dc5fd82f788"
   pkg.url "http://buildsources.delivery.puppetlabs.net/#{pkg.get_name}-#{pkg.get_version}.tar.gz"
 
-  pkg.build_requires "pl-gcc"
-  pkg.build_requires "make"
+  if platform.is_aix?
+    pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-gcc-5.2.0-1.aix#{platform.os_version}.ppc.rpm"
+    pkg.environment "PATH" => "/opt/pl-build-tools/bin:$$PATH"
+  else
+    pkg.build_requires "pl-gcc"
+    pkg.build_requires "make"
+    pkg.environment "LDFLAGS" => "-Wl,-rpath=#{settings[:libdir]} -L#{settings[:libdir]}"
+    pkg.environment "CFLAGS" => "$${CFLAGS} -fPIC"
+  end
 
-  pkg.environment "LDFLAGS" => "-Wl,-rpath=#{settings[:libdir]} -L#{settings[:libdir]}"
-  pkg.environment "CFLAGS" => "$${CFLAGS} -fPIC"
 
   pkg.configure do
     ["./configure --prefix=#{settings[:prefix]} --without-python"]
