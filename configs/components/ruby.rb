23bca3b24335463d84c9995752303e8a754f43f6
Michael Stahnke <stahnma@puppetlabs.com>
2016-05-16 21:18:49 +0000

    Add ruby 2.3.1

--- configs/components/ruby.rb
+++ configs/components/ruby.rb
@@ -1,6 +1,7 @@
 component "ruby" do |pkg, settings, platform|
-  pkg.version "2.1.9"
-  pkg.md5sum "d9d2109d3827789344cc3aceb8e1d697"
+  pkg.version "2.3.1"
+  pkg.md5sum "0d896c2e7fd54f722b399f407e48a4c6"
+  pkg.url "http://buildsources.delivery.puppetlabs.net/ruby-#{pkg.get_version}.tar.gz"
   pkg.url "https://cache.ruby-lang.org/pub/ruby/2.1/ruby-#{pkg.get_version}.tar.gz"
 
   if platform.is_windows?
@@ -17,7 +18,7 @@ component "ruby" do |pkg, settings, platform|
   pkg.replaces 'pe-rubygem-gem2rpm'
 
   base = 'resources/patches/ruby'
-  pkg.apply_patch "#{base}/libyaml_cve-2014-9130.patch"
+  #pkg.apply_patch "#{base}/libyaml_cve-2014-9130.patch"
 
   # These are a pretty smelly hack, and they run the risk of letting tests
   # based on the generated data (that should otherwise fail) pass
